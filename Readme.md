48000bc63442ad981c96e84546a9177eb6324668
Luis Guilherme Vieira Neves <luisguilhermevnevesnogueira@gmail.com>
2022-04-17 10:41:20 -0300

    Adição do diagrama + pagina play

---
+++ Readme.md
@@ -0,0 +1,7 @@
+## Módulo 1 - Projeto 3 One Chance
+
+> Comece pelo arquivo play.html
+
+### Diagrama do Projeto:
+
+![](Diagrama.png)
---
+++ js/play.js
@@ -0,0 +1,10 @@
+function comecarJogo(){
+  // Referência: https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event
+
+  /*
+    - Assim que o <h1> da página play for clicado, a função comecarJogo()
+    vai ser responsável por simular como se fosse uma "rota" no express,
+    abrindo minha página 1_comeco.html em outra aba ('_blank')
+  */
+  window.open('./html/1_comeco.html', '_blank')
+}
